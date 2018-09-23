7c2039a5a96e258ed0765c744c7532ea9bf9043f
ARDITYAYS <220499yogaputa@gmail.com>
2018-09-23 21:33:21 +0700

    first commit

---
+++ src/pbo310117119latihan15/PBO310117119Latihan15.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo310117119latihan15;
+/**
+ * @author  
+ * Nama      : Arditya Yogaputra S
+ * Kelas     : IF-3 / PBO-3
+ * Nim       : 10117104
+ * Deskripsi : Program ini menampilkan Operator Logika 
+ */
+public class PBO310117119Latihan15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = "+(a && b));
+        System.out.println("a || b = "+(a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a&&b)" + !(a&&b));
+    }
+    
+}
